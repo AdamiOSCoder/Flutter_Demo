c4bdef5a8c71c7c698b247ed06f24f210dc8d681
damXEve <764568929@qq.com>
2020-07-26 19:53:59 +0900

    FlutterDemo25 对话框

---
+++ FlutterDemo25 对话框/lib/main.dart
@@ -0,0 +1,128 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: MyHomePage(title: '对话框'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+          child: Column(
+        children: [
+          RaisedButton(
+              child: Text('AlertDialog'),
+              onPressed: () async {
+                //弹出对话框并等待其关闭
+                bool delete = await _showDeleteConfirmDialog1();
+                if (delete == null) {
+                  print("取消删除");
+                } else {
+                  print("已确认删除");
+                  //... 删除文件
+                }
+              }),
+          RaisedButton(
+              child: Text('SimpleDialog'),
+              onPressed: () async {
+                //弹出对话框并等待其关闭
+                bool delete = await _changeLanguage();
+                if (delete == null) {
+                  print("取消删除");
+                } else {
+                  print("已确认删除");
+                  //... 删除文件
+                }
+              }),
+        ],
+      )),
+    );
+  }
+
+  // 弹出对话框 AlertDialog
+  Future<bool> _showDeleteConfirmDialog1() {
+    return showDialog<bool>(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text("提示"),
+          content: Text("您确定要删除当前文件吗?"),
+          actions: <Widget>[
+            FlatButton(
+              child: Text("取消"),
+              onPressed: () => Navigator.of(context).pop(), // 关闭对话框
+            ),
+            FlatButton(
+              child: Text("删除"),
+              onPressed: () {
+                //关闭对话框并返回true
+                Navigator.of(context).pop(true);
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  // 弹出对话框 SimpleDialog
+  Future<bool> _changeLanguage() async {
+    int i = await showDialog<int>(
+        context: context,
+        builder: (BuildContext context) {
+          return SimpleDialog(
+            title: const Text('请选择语言'),
+            children: <Widget>[
+              SimpleDialogOption(
+                onPressed: () {
+                  // 返回1
+                  Navigator.pop(context, 1);
+                },
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 6),
+                  child: const Text('中文简体'),
+                ),
+              ),
+              SimpleDialogOption(
+                onPressed: () {
+                  // 返回2
+                  Navigator.pop(context, 2);
+                },
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 6),
+                  child: const Text('美国英语'),
+                ),
+              ),
+            ],
+          );
+        });
+
+    if (i != null) {
+      print("选择了：${i == 1 ? "中文简体" : "美国英语"}");
+    }
+  }
+}
